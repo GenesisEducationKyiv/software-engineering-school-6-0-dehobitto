560d17a31ce2a68ac97ea3579748f06ba8b0cb7f
dehobitto <dehobitto@dadgafk@gmail.com>
2026-05-03 17:28:54 +0300

    refactor: main.go variable names renamed to make it easier to comprehend

--- main.go
+++ main.go
@@ -12,32 +12,32 @@ import (
 func main() {
 	cfg := config.LoadConfig()
 
-	pool, err := database.Connect(cfg)
+	connectionPool, err := database.Connect(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer pool.Close()
+	defer connectionPool.Close()
 
-	err = database.Migrate(pool, "infra/database/schemas.sql")
+	err = database.Migrate(connectionPool, cfg.SchemasPath)
 	if err != nil {
 		log.Fatalf("Migration failed: %v", err)
 	}
 
-	repo := database.NewRepository(pool)
+	repo := database.NewRepository(connectionPool)
 
-	rc := cache.NewRedisCache(cfg.RedisAddr)
+	redisCache := cache.NewRedisCache(cfg.RedisAddr)
 
 	jobsChannel := make(chan workers.NotificationJob, 100)
 
 	notifier := workers.NewNotifierWorker(cfg)
 	go notifier.Start(jobsChannel)
 
-	scanner := workers.NewScannerWorker(repo, cfg, jobsChannel, rc)
+	scanner := workers.NewScannerWorker(repo, cfg, jobsChannel, redisCache)
 	go scanner.StartScanner()
 
-	r := routes.SetupRouter(repo, cfg, jobsChannel, rc)
+	router := routes.SetupRouter(repo, cfg, jobsChannel, redisCache)
 
-	if err = r.Run(":" + cfg.ServerPort); err != nil {
+	if err = router.Run(":" + cfg.ServerPort); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
--- models/subscription.go
+++ models/subscription.go
@@ -4,6 +4,6 @@ type Subscription struct {
 	Email       string `json:"email"`         // Email address
 	Repo        string `json:"repo"`          // GitHub repository in owner/repo format
 	Confirmed   bool   `json:"confirmed"`     // Whether the subscription is confirmed
-	LastSeenTag string `json:"last_seen_tag"` // Last seen release tag for this repository
+	LastSeenTag string `json:"last_seen_tag"` // Last seen release tag for "Repo"
 	Token       string `json:"-"`
 }
