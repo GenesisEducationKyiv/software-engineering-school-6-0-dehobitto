e9edf8d3974a3c1a5708da1a94b4d2cb3ee7e4d6
dehobitto <dehobitto@dadgafk@gmail.com>
2026-05-29 15:12:37 +0300

    refactor(workers): inline ScanRepository into scanner.go

--- internal/workers/scanner.go
+++ internal/workers/scanner.go
@@ -13,6 +13,12 @@ import (
 	"subber/internal/models"
 )
 
+type ScanRepository interface {
+	GetUniqueSubscriptions(ctx context.Context) ([]models.GitHubRelease, error)
+	GetSubscribers(ctx context.Context, repo string) ([]string, error)
+	UpdateTags(ctx context.Context, repo models.GitHubRelease) error
+}
+
 type ScannerWorker struct {
 	repo   ScanRepository
 	cfg    *config.Config
